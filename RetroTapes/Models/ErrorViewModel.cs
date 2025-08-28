d9bd232640ccbf724af8a8ba4b9ec38e7d15c661
alsaf <alsaf@HP>
2025-08-28 13:09:47 +0200

    Add project files.

---
+++ RetroTapes/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RetroTapes.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
