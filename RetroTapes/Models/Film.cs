fd4653b008bf4a5da1a959aca3711b944f414326
Filippa Aderot <filippa.aderot@icloud.com>
2025-09-30 12:29:15 +0200

    ändrade variabeln i release year och length så det matchade databasen, nu fungerar film info sidan

--- RetroTapes/Models/Film.cs
+++ RetroTapes/Models/Film.cs
@@ -20,7 +20,7 @@ namespace RetroTapes.Models
         public string? Description { get; set; }
 
         [Column("release_year")]
-        public int? ReleaseYear { get; set; }
+        public string? ReleaseYear { get; set; }
 
         [Required]
         [Column("language_id")]
@@ -38,7 +38,7 @@ namespace RetroTapes.Models
         public decimal RentalRate { get; set; }
 
         [Column("length")]
-        public int? Length { get; set; }
+        public short? Length { get; set; }
 
         [Required]
         [Column("replacement_cost")]
@@ -55,7 +55,5 @@ namespace RetroTapes.Models
         [MaxLength(255)]
         public string? SpecialFeatures { get; set; }
 
-        [Column("fulltext")]
-        public string? FullText { get; set; }
     }
 }
